b480ac2a38342a5110552aff44200e2326227daf
Ahmad-AbdElrahman <ahmedabdelrhman433@gmail.com>
2023-10-05 14:26:00 -0700

    Programming is like building a multilingual puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-/** Write a C program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line. */
+/** Write a C program that prints exactly with proper grammar, but the outcome is a piece of art */
 
 int main(void)
-{
+{ /** Printing strings with return [0] */
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
