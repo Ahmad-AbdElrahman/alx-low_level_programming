7eadbd0eac79dbc576071e8e3136dcf411843e74
Ahmad-AbdElrahman <ahmedabdelrhman433@gmail.com>
2023-10-05 14:41:46 -0700

    Printff

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+/*
+ * File: 5-printf.c
+ * Auth: Ahmed Hassan <ahmedabdelrhman433@gmail.com>
+ */
+
+#include <stdio.h>
+
+/**
+ * main -  Write a C program that prints exactly with proper grammar, but the outcome is a piece of art
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
