53274ee741215f57b3fa0ea4d9f82db41603b0a9
Ahmad-AbdElrahman <ahmedabdelrhman433@gmail.com>
2023-10-08 13:52:47 -0700

    Last digit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+/*
+ * File: 2-print_alphabet.c
+ * Auth: Ahmed Hassan <ahmedabdelrhman433@gmail.com>
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints The alphabet in lower case
+ * Return: 0
+ */
+
+int main(void)
+{
+	char ch;
+	
+	for(ch = 'a' ; ch <= 'z' ; ch++)
+	{
+      		putchar(ch);
+	}
+	putchar("\n");
+	return (0);
+}
