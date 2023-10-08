af853c5b5e1bf71ad40161b6280220683c2e4624
Ahmad-AbdElrahman <ahmedabdelrhman433@gmail.com>
2023-10-08 13:12:05 -0700

    If Statment

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,34 @@
+/*
+ * File: 0-positive_or_negative.c
+ * Auth: Ahmed Hassan <ahmedabdelrhman433@gmail.com>
+ */
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Positive anything is better than negative nothing
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("%d ", n);
+	if (n > 0) 
+	{
+		printf("is positive \n");
+	}
+	else if (n == 0)
+	{
+		printf("is zero \n");
+	}
+	else
+	{	
+		printf("is negative \n");
+	}
+	return (0);
+}
