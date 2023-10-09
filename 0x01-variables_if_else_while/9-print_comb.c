b68eb60689729e11c0fea26c45a208f5119c0b8f
Ahmad-AbdElrahman <ahmedabdelrhman433@gmail.com>
2023-10-09 12:19:37 -0700

    print numbers combo

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+/*
+ * File: 9-print_comb.c
+ * Auth: Ahmed Hassan <ahmedabdelrhman433@gmail.com>
+ */
+
+#include <stdio.h>
+
+/**
+ * main - program that prints all possible combinations of single-digit numbersi
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		putchar('0' + i);
+		if (i == 9)
+			break;
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
