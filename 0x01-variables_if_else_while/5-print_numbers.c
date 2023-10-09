d33eac1978f9ab083d6e918856d4bf016f2c0883
Ahmad-AbdElrahman <ahmedabdelrhman433@gmail.com>
2023-10-09 11:36:47 -0700

    printing singl numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+/*
+ * File: 5-print_numbers.c
+ * Auth: Ahmed Hassan <ahmedabdelrhman433@gmail.com>
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Write a program that prints all single digit numbers of 
+ * base 10 starting from 0
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	for (i=0 ; i<10 ; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
