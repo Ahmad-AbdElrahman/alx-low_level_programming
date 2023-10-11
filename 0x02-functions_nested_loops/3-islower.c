4bd9fdcdc68de3e85e7318ba4b8af0b9f9ef71b1
Ahmad-AbdElrahman <ahmedabdelrhman433@gmail.com>
2023-10-10 19:38:53 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+/*
+ * File: 3-islower.c
+ * Auth: Ahmed Hassan <ahmedabdelrhman433@gmail.com>
+ */
+
+#include "main.h"
+
+/**
+ * _islower - Prints function that prints the lower alphabet
+ * Return: 0
+ */
+
+int _islower(int c)
+{
+	char c;
+
+	if (c >= 97 && <= 122)
+	{
+		return (1);
+	}
+
+	return (0)
+}
