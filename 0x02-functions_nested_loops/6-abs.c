a0135aa65f7d04f92ead10dea82040c7c471f1f3
Ahmad-AbdElrahman <ahmedabdelrhman433@gmail.com>
2023-10-11 12:59:26 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,27 @@
+/*
+ * File: 6-abs.c
+ * Auth: Ahmed Hassan <ahmedabdelrhman433@gmail.com>
+ */
+
+#include "main.h"
+
+/**
+ * _abs(int) - Write a function that checks for alphabet
+ * Return: 0
+ */
+
+int _abs(int)
+{
+	int n;
+
+	if (n < 0)
+	{
+		r = n * -1;
+		_putchar(r);
+	}
+	}
+	else
+	{
+		_putchar(n);
+	}
+}
