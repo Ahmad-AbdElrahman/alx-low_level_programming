369523b1ec8464ec2ec7be8245ceb1f4dadbbe8a
Ahmad-AbdElrahman <ahmedabdelrhman433@gmail.com>
2023-10-11 13:36:54 -0700

    natural numbers

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,50 @@
+/*
+ * File: 11-print_to_98.c
+ * Auth: Ahmed Hassan <ahmedabdelrhman433@gmail.com>
+ */
+
+#include "main.h"
+
+/**
+ * print_to_98 - function that prints all natural numbers from n to 98
+ * followed by a new line
+ * @i: The first number to be added
+ * @k: The first number to be added
+ * Return: 0
+ */
+
+void print_to_98(int n)
+{
+		if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			if (n == 98)
+			{
+				printf("%d", n);
+				printf("\n");
+				break;
+			}
+			else
+			{
+				printf("%d, ", n);
+			}
+		}
+	}
+	else
+	{
+		for (; n >= 98; n--)
+		{
+			if (n == 98)
+			{
+				printf("%d", n);
+				printf("\n");
+				break;
+			}
+			else
+			{
+				printf("%d, ", n);
+			}
+		}
+	}
+}
