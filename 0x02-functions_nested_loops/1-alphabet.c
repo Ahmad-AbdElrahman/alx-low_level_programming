7fe8729cef183faa90fef9d851935ee6d24b5150
Ahmad-AbdElrahman <ahmedabdelrhman433@gmail.com>
2023-10-10 19:16:45 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+/*
+ * File: 1-alphabet.c
+ * Auth: Ahmed Hassan <ahmedabdelrhman433@gmail.com>
+ */
+
+#include "main.h"
+
+/**
+ * main - Prints function that prints the alphabet
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 'a' ; i < 'z' ; i++)
+	{
+		_putchar('i');
+	}
+	_puthar('\n');
+	return (0);
+}
