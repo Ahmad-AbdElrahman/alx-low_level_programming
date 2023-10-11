64f69e6e35014078b00fa27754accfb1d1c1ed9d
Ahmad-AbdElrahman <ahmedabdelrhman433@gmail.com>
2023-10-10 19:27:13 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+/*
+ * File: 2-print_alphabet_x10.c
+ * Auth: Ahmed Hassan <ahmedabdelrhman433@gmail.com>
+ */
+
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints function that prints the alphabet
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
