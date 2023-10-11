6d3a576380ad196d4de8623164c1a89b63d286b7
Ahmad-AbdElrahman <ahmedabdelrhman433@gmail.com>
2023-10-11 12:38:53 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,36 @@
+/*
+ * File: 5-sign.c
+ * Auth: Ahmed Hassan <ahmedabdelrhman433@gmail.com>
+ */
+
+#include "main.h"
+
+/**
+ * print_sign - Write a function that checks for alphabet
+ * Return: 0
+ * @n: The character to be checked
+ */
+
+int print_sign(int n)
+{
+	if (n < 0)
+	{
+		_putchar('+');
+		_putchar(',');
+		_putchar(' ');
+		return (1);
+	}
+	else if (n > 0)
+	{
+		_putchar('-');
+		_putchar(',');
+		_putchar(' ');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		return (0);
+}
