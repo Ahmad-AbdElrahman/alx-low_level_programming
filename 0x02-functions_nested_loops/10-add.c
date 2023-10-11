195bd23aeec743c5500cf8536f3ab03858690e59
Ahmad-AbdElrahman <ahmedabdelrhman433@gmail.com>
2023-10-11 13:31:24 -0700

    adding

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+/*
+ * File: 10-add.c
+ * Auth: Ahmed Hassan <ahmedabdelrhman433@gmail.com>
+ */
+
+#include "main.h"
+
+/**
+ * add - Write a function that adds two integers and returns the result
+ * Return: 0
+ */
+
+int add(int i, int k)
+{
+	return (i + k);
+}
