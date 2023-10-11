71cf6b66731fa4ccf0fb8fbdac89dc7d21665fbc
Ahmad-AbdElrahman <ahmedabdelrhman433@gmail.com>
2023-10-11 13:22:57 -0700

    24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,9 +6,8 @@
 #include "main.h"
 
 /**
- * jack_bauer - prints every minute of the day of Jack Bauer, 
+ * jack_bauer - prints every minute of the day of Jack Bauer,
  * starting from 00:00 to 23:59.
- * @n: The number to be computed.
  * Return: 0
  */
 
