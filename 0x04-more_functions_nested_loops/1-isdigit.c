cd311f0ada99d731e98399a5bf206b5e4a09be91
Ahmad-AbdElrahman <ahmedabdelrhman433@gmail.com>
2023-10-13 05:13:24 -0700

    checks for integer

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,24 @@
+/*
+ * File: 1-isdigit.c
+ * Auth: Ahmed Hassan <ahmedabdelrhman433@gmail.com>
+ */
+
+#include "main.h"
+
+/**
+ * main - Prints Write a function that checks for a digit (0 through 9)
+ * @c: The number to be checked
+ * Return: 0
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
