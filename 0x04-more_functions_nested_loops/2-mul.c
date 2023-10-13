af4a99064f385da71e84400a790761f094141a13
Ahmad-AbdElrahman <ahmedabdelrhman433@gmail.com>
2023-10-13 07:15:38 -0700

    multiplication

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -7,9 +7,9 @@
 
 /**
  * mul - Prints Write a function that multiplies two integers
- * @a the first number
- * @b the second number
- * Return: 0
+ * @a: the first number
+ * @b: the second number
+ * Return: The result of multiplication between a and b
  */
 
 int mul(int a, int b)
