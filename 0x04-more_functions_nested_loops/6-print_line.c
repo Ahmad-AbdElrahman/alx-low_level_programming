a789024fc3bd1a4cd3c468731b06b2ba6af297a2
Ahmad-AbdElrahman <ahmedabdelrhman433@gmail.com>
2023-10-13 10:44:10 -0700

    draw line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,28 @@
+/*
+ * File: 6-print_line.c
+ * Auth: Ahmed Hassan <ahmedabdelrhman433@gmail.com>
+ */
+
+#include "main.h"
+
+/**
+ * print_line - Write a function that draws a straight line in the terminal
+ * Return: Draw straight line
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (n <= 1)
+		{
+			_putchar('\n');
+		}
+		else
+		{
+			_putchar('-');
+		}
+	}
+		_putchar('\n');
+}
