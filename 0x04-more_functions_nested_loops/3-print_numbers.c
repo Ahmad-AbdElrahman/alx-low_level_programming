b75ad6fc2afdce7166a8c288a6e69ce23c24cd03
Ahmad-AbdElrahman <ahmedabdelrhman433@gmail.com>
2023-10-13 07:51:04 -0700

    0 to 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,8 +6,8 @@
 #include "main.h"
 
 /**
- * print_numbers - Write a function that prints the numbers, from 0 to 9
- * , followed by a new line
+ * print_numbers - Write a function that prints the numbers
+ * followed by a new line
  * Return: 
  */
 
