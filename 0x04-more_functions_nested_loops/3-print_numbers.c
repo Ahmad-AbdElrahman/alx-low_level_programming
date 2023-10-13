8214059e3517d755d7917beaa34ff9713b83acd0
Ahmad-AbdElrahman <ahmedabdelrhman433@gmail.com>
2023-10-13 07:41:21 -0700

    0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,23 @@
+/*
+ * File: 3-print_numbers.c
+ * Auth: Ahmed Hassan <ahmedabdelrhman433@gmail.com>
+ */
+
+#include "main.h"
+
+/**
+ * print_numbers - Write a function that prints the numbers, from 0 to 9
+ * , followed by a new line
+ * Return: 
+ */
+
+void print_numbers(void)
+{
+	int i:
+	
+	for (i = 0 ; i < 10 ; ++1)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
