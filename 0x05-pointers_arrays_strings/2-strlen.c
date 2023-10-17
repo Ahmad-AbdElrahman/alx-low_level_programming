d506a90d75c92b14604998d3c15973b691ee77d9
Ahmad-AbdElrahman <ahmedabdelrhman433@gmail.com>
2023-10-16 19:26:35 -0700

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+/*
+ * File: 2-strlen.c
+ * Auth: Ahmed Hassan <ahmedabdelrhman433@gmail.com>
+ */
+
+#include "main.h"
+
+/**
+ * _strlen - Write a function that returns the length of a string
+ * @s: string
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (; *s != '\0' ; s++)
+	{
+		i++;
+	}
+	return (i);
+}
