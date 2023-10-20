b83b77015b7cb0023b3f8f2b4ebf0391ac2e71e0
Ahmed Hassan <ahmedabdelrhman433@gmail.com>
2023-10-20 05:28:51 +0300

    appending string

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -5,7 +5,6 @@
   * _strncat - Concatenates two strings
   * @dest: The destination string
   * @src: The source string
-  *
   * Return: A pointer to the resulting string dest
   */
 char *_strncat(char *dest, char *src, int n)
@@ -16,13 +15,14 @@ char *_strncat(char *dest, char *src, int n)
 	{
 		dlen++;
 	}
+
 	while (i < n && src[i])
 	{
 		dest[dlen] = src[i];
 		dlen++;
 		i++;
 	}
-
 	dest[dlen + n + 1] = '\0';
+
 	return (dest);
 }
