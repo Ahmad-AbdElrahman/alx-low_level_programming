4301be377851233523b3639d4058347080703399
Ahmad-AbdElrahman <ahmedabdelrhman433@gmail.com>
2023-10-27 17:08:06 -0700

    program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+/*
+ * File: 0-whatsmyname.c
+ * Auth: Ahmed Hassan <ahmedabdelrhman433@gmail.com>
+ */
+
+#include <stdio.h>
+int main(int argc, char *argv[])
+{
+	argc = argc;
+	
+	printf("%s\n", argv[0]);
+	return(0);
+}
