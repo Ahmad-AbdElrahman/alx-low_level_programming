98485b3dba02108c9b4860ea0b3c20d585e19b3f
Ahmad-AbdElrahman <ahmedabdelrhman433@gmail.com>
2023-10-27 17:38:52 -0700

    mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main - Print mul of two numbers
+  * @argc: argument count
+  * @argv: argument vector
+  *
+  * Return: |ero for success
+  */
+int main(int argc, char *argv[])
+{
+	int i, k;
+
+	if(argc < 3)
+	{
+		printf("Error\n");
+		return(1);
+	}
+	else
+	{
+		i = atoi(argv[1]);
+		k = atoi(argv[2]);
+		printf("%d\n", i * k);
+	}
+	return (0);
+}
