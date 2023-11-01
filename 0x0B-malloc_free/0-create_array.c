6531c82665e9f273018211acabcc7e839ea404cd
Ahmed Hassan <ahmedabdelrhman433@gmail.com>
2023-11-01 21:06:44 +0200

    malloc

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,16 @@
+/*
+ * File: 0-create_array.c
+ * Auth: Ahmed Hassan <ahmedabdelrhman433@gmail.com>
+ */
+
+#include <stdio.h>
+/**
+ * create_array - Prints Write a function that multiplies two integers
+ * @size: array size
+ * @c: string
+ * Return: memory allocation
+ */
+char *create_array(unsigned int size, char c)
+{
+    c = malloc(size * sizeof(char));
+}
