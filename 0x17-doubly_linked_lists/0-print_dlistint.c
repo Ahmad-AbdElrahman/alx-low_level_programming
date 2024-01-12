3dc0513ef94447c8b60d1b969b0a8e9a737bff49
Ahmed Hassan <ahmedabdelrhman433@gmail.com>
2024-01-12 22:38:55 +0200

    0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,20 +7,21 @@
  * @h: pointer to struct
  * Return: the number of nodes.
  */
-
 size_t print_dlistint(const dlistint_t *h)
 {
 	const dlistint_t *current;
 	int n = 0;
-
-	current = h;
-	while(current->next != NULL)
-	{
-		printf("%i\n", current->n);
-		current = current->next;
+	
+	if(h == NULL)
+		return 0;
+		current = h;
+		while(current->next != NULL)
+		{
+			printf("%i\n", current->n);
+			current = current->next;
+			n++;
+		}
 		n++;
-	}
-	n++;
-	printf("%i\n", current->n);
-	return n;
+		printf("%i\n", current->n);
+		return n;
 }
