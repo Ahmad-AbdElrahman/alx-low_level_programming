0cee9ebdc1f9583d7c00305161063b817b0c1da0
Ahmed Hassan <ahmedabdelrhman433@gmail.com>
2024-01-12 23:22:37 +0200

    3

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code
+ * @head: head of struct
+ * @n: data of node
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return NULL;
+	new->n = n;
+	new->prev = NULL;
+	new->next = (*head);
+	
+	if (*head != NULL)
+		(*head)->prev = new;
+	
+	*head = new
+
+	return (new);
+}
